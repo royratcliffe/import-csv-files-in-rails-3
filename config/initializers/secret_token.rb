9e05e07b6ce7406a504a4624814aa0703f32da15
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2010-07-13 16:24:11 +0100

    Rails 3 basic application
    Generated using
    	rails new import-csv-files-in-rails-3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'e9e942d86b4563caa5ea7b27ab676bde8c404a91de938f63fabd01d26ad9a1b12400dc0d4f2204beb50a12006e194d024c9ad2a022735d6ad6fb348bb2e68c36'
