28eb813420c0a1f6aef9c6c03ead170f26eb3be9
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2010-07-15 14:06:57 +0100

    Migration, schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100713170623) do
+ActiveRecord::Schema.define(:version => 20100715124738) do
+
+  create_table "import_cells", :force => true do |t|
+    t.integer  "import_table_id"
+    t.integer  "row_index"
+    t.integer  "column_index"
+    t.string   "contents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "import_cells", ["import_table_id"], :name => "index_import_cells_on_import_table_id"
+
+  create_table "import_tables", :force => true do |t|
+    t.string   "original_path"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "people", :force => true do |t|
     t.string   "first_name"
